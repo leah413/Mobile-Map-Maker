aba407ba0426a1155700177debf81fab4435d0ad
Leah Nella <leahnella@yahoo.com>
2019-07-10 10:59:15 -0500

    Added Geocoder

--- Mobile Map Maker/ViewController.swift
+++ Mobile Map Maker/ViewController.swift
@@ -15,6 +15,8 @@ class ViewController: UIViewController {
     @IBOutlet weak var mapView: MKMapView!
     let northwesternAnnotation = MKPointAnnotation()
     let locationmanager = CLLocationManager()
+    let address = "Mount Rushmore"
+    let geocoder = CLGeocoder()
     override func viewDidLoad() {
         super.viewDidLoad()
         let latitude = 42.055984
@@ -25,6 +27,14 @@ class ViewController: UIViewController {
         northwesternAnnotation.coordinate = coordinate
         northwesternAnnotation.title = "Northwestern University"
         mapView.addAnnotation(northwesternAnnotation)
+        geocoder.geocodeAddressString(address) { (placemarks, error) in
+            for placemark in placemarks! {
+                let annotation = MKPointAnnotation()
+                annotation.coordinate = (placemark.location?.coordinate)!
+                annotation.title = placemark.name
+                self.mapView.addAnnotation(annotation)
+            }
+        }
        
     }
 
